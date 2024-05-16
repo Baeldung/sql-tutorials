90d37ea79c3df72f9c58274ee35f40561db6d9d4
amirsarb <amir.sarbazi@gmail.com>
2024-05-16 19:48:39 +1000

    Efficiently Insert Multiple Rows

---
+++ SQL-Insert-Multiple-Rows/insert-multiple-rows.sql
@@ -0,0 +1,28 @@
+
+CREATE TABLE old_student (
+    id INT PRIMARY KEY,
+    name VARCHAR(255),
+    national_id VARCHAR(255),
+    graduation_date DATE
+)
+
+INSERT INTO student (id, name, national_id)
+VALUES (1001, 'John Liu', '123345566'),
+       (1003, 'Rita Ora', '132345166'),
+       (1007, 'Philip Lose', '321345566'),
+       (1010, 'Samantha Prabhu', '3217165566');
+
+INSERT INTO student (id, name, national_id)
+SELECT id, name, national_id
+FROM old_student
+WHERE graduation_date = '2024-06-15';
+
+
+INSERT INTO student (id, name, national_id) 
+VALUES (1001, 'John Liu', 123345566) 
+UNION ALL 
+VALUES (1003, 'Rita Ora', 132345166) 
+UNION ALL 
+VALUES (1007, 'Philip Lose', 321345566)
+UNION ALL 
+VALUES (1010, 'Samantha Prabhu', 3217165566)
