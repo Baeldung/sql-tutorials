1771cf3c71ca5ac071faf2ad8eeb4dc45e1423a3
Yadukrishnan <yadavan88@gmail.com>
2024-05-18 19:28:15 +0200

    Added insert script

--- sql-queries/null-empty-filter/null-empty.sql
+++ sql-queries/null-empty-filter/null-empty.sql
@@ -1,3 +1,8 @@
+INSERT INTO Department VALUES(1, 'Computer Science', 'CS'); 
+INSERT INTO Department VALUES(2, 'Mechanical Engineering', 'ME'); 
+INSERT INTO Department VALUES(3, 'Psychology', ''); 
+INSERT INTO Department VALUES(4, 'Statistics', NULL);
+
 SELECT id, name from Department WHERE code IS NULL OR code = '';
 SELECT id, name from Department WHERE code IS NULL OR TRIM(code) = '';
 SELECT id, name from Department WHERE TRIM(COALESCE(code, '')) = '';
