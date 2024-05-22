c4cc34d1359751dc04ad17889dfce6291996ab95
Haroon Javed <141476933+haroon445@users.noreply.github.com>
2024-05-22 23:47:43 +0500

    Delete sql-queries/sql-if-then/if-query-mysql.sql

--- sql-queries/sql-if-then/if-query-mysql.sql
+++
@@ -1,8 +0,0 @@
-SELECT student_id, course_id,
-IF(grade = 'A+', 'SUPER',
-IF(grade = 'A', 'EXCELLENT',
-IF(grade = 'B+', 'GOOD',
-IF(grade = 'B', 'SATISFACTORY',
-IF(grade = 'C', 'NEEDS IMPROVEMENT', 'POOR'))))
-) AS grade_remarks
-FROM EXAM;
