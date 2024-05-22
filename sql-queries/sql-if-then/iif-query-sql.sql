2a68409cd1f2fdf41e8e44dd45cd05b6f0371d2d
Haroon Javed <141476933+haroon445@users.noreply.github.com>
2024-05-23 00:11:52 +0500

    Delete sql-queries/sql-if-then/iif-query-sql.sql

--- sql-queries/sql-if-then/iif-query-sql.sql
+++
@@ -1,8 +0,0 @@
-SELECT student_id, course_id,
-IIF(grade = 'A+', 'SUPER',
-IIF(grade = 'A', 'EXCELLENT',
-IIF(grade = 'B+', 'GOOD',
-IIF(grade = 'B', 'SATISFACTORY',
-IIF(grade = 'C', 'NEEDS IMPROVEMENT', 'POOR'))))
-) AS grade_remarks
-FROM EXAM;
