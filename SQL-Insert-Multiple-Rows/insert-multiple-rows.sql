6b5451eebb86c99c8475ea672f61bed349a6f4cd
amirsarb <amir.sarbazi@gmail.com>
2024-05-26 10:36:00 +1000

    Capital Letter Fixed

--- SQL-Insert-Multiple-Rows/insert-multiple-rows.sql
+++ SQL-Insert-Multiple-Rows/insert-multiple-rows.sql
@@ -1,24 +1,24 @@
 
-CREATE TABLE old_student (
+CREATE TABLE Old_student (
     id INT PRIMARY KEY,
     name VARCHAR(255),
     national_id VARCHAR(255),
     graduation_date DATE
 )
 
-INSERT INTO student (id, name, national_id)
+INSERT INTO Student (id, name, national_id)
 VALUES (1001, 'John Liu', '123345566'),
        (1003, 'Rita Ora', '132345166'),
        (1007, 'Philip Lose', '321345566'),
        (1010, 'Samantha Prabhu', '3217165566');
 
-INSERT INTO student (id, name, national_id)
+INSERT INTO Student (id, name, national_id)
 SELECT id, name, national_id
 FROM old_student
 WHERE graduation_date = '2024-06-15';
 
 
-INSERT INTO student (id, name, national_id) 
+INSERT INTO Student (id, name, national_id) 
 VALUES (1001, 'John Liu', 123345566) 
 UNION ALL 
 VALUES (1003, 'Rita Ora', 132345166) 
