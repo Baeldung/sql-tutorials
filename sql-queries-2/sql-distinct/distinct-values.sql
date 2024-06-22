dd45b099fb740cab4e9a3ca81d121288114323c3
sukstechs2311 <sukstechs2311@gmail.com>
2024-06-22 16:12:49 -0700

    Create distinct-values.sql

---
+++ sql-queries-2/sql-distinct/distinct-values.sql
@@ -0,0 +1,8 @@
+
+SELECT * FROM Employees;
+
+SELECT COUNT(DISTINCT department) AS uniq_dept FROM Employees;
+
+SELECT department, COUNT(*) FROM employees GROUP BY department;
+
+SELECT department, COUNT(*) FROM employees GROUP BY department ORDER BY count DESC;
