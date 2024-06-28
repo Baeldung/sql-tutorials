72ccf5e53aea641b6e3dda83a8f238315b3a8b2e
Yadukrishnan <yadavan88@gmail.com>
2024-06-28 21:20:28 +0200

    Prepared the tables and data for simple profile

--- 1-setup/schema/simple/university-postgresql.sql
+++ 1-setup/schema/simple/university-postgresql.sql
@@ -22,35 +22,6 @@ CREATE TABLE Department
     UNIQUE (id)
 );
 
-
-CREATE TABLE Program
-(
-    id INT PRIMARY KEY NOT Null,
-    name VARCHAR (50),
-    description VARCHAR (250),
-    start_date DATE,
-    end_date DATE,
-    type VARCHAR (20),
-    department_id INT,
-    CONSTRAINT program_department_id_fkey FOREIGN KEY(department_id) REFERENCES Department(id),
-    UNIQUE (id)
-);
-
-
-CREATE TABLE Student
-(
-    id INT PRIMARY KEY NOT null,
-    name VARCHAR (60),
-    national_id BIGINT NOT Null, 
-    birth_date DATE,
-    enrollment_date DATE,
-    graduation_date DATE,
-    gpa REAL,
-    UNIQUE (id)
-);
-
-
-
 CREATE TABLE Faculty
 (
     id INT PRIMARY KEY NOT Null,
@@ -75,71 +46,5 @@ CREATE TABLE Course
     is_active VARCHAR(10),
     department_id INT,
     CONSTRAINT course_department_id_fkey FOREIGN KEY(department_id) REFERENCES Department(id),
-    UNIQUE (id)
-);
-
-
-CREATE TABLE Teaching
-( 
-    id INT PRIMARY KEY NOT Null,
-    role VARCHAR(60),
-    semester VARCHAR(30),
-    year INT,
-    course_id VARCHAR (10),
-    faculty_id INT,
-    CONSTRAINT teaching_course_id_fkey FOREIGN KEY(course_id) REFERENCES Course(id),
-    CONSTRAINT teaching_faculty_id_fkey FOREIGN KEY(faculty_id) REFERENCES Faculty(id),
-    UNIQUE (id)
-);
-
-
-CREATE TABLE Specification
-(
-    id INT PRIMARY KEY NOT Null,
-    status VARCHAR (20),
-    course_id VARCHAR (10),
-    program_id INT,
-    CONSTRAINT specification_course_id_fkey FOREIGN KEY(course_id) REFERENCES Course(id),
-    CONSTRAINT specification_program_id_fkey FOREIGN KEY(program_id) REFERENCES Program(id),
-    UNIQUE (id)
-);
-
-
-
-CREATE TABLE Prerequisite
-(
-    id INT PRIMARY KEY NOT Null,
-    course_id VARCHAR (10),
-    prerequisite_id VARCHAR (10) not null,
-    CONSTRAINT prerequisite_course_id_fkey FOREIGN KEY(course_id) REFERENCES Course(id),
-    CONSTRAINT prerequisite_prerequisite_id_fkey FOREIGN KEY(prerequisite_id) REFERENCES Course(id),
-    UNIQUE (id)
-);
-
-
-CREATE Table Registration
-(
-    id INT PRIMARY KEY NOT Null,
-    semester VARCHAR(30),
-    year INT,
-    reg_datetime TIMESTAMP,
-    course_id VARCHAR (10),
-    student_id INT,
-    CONSTRAINT registration_course_id_fkey FOREIGN KEY(course_id) REFERENCES Course(id),
-    CONSTRAINT registration_student_id_fkey FOREIGN KEY(student_id) REFERENCES Student(id),
-    UNIQUE (id)
-);
-
-
-CREATE TABLE Exam
-(
-    id INT PRIMARY KEY NOT Null,
-    semester VARCHAR(30),
-    exam_date DATE,
-    grade VARCHAR(2),
-    course_id VARCHAR (10),
-    student_id INT,
-    CONSTRAINT exam_student_id_fkey FOREIGN KEY(student_id) REFERENCES Student(id),
-    CONSTRAINT exam_course_id_fkey FOREIGN KEY(course_id) REFERENCES Course(id),
     UNIQUE (id)
 );
