5d180111aaa6afb55d45a6f3c3cb6e10dc92c3c2
technoddy <mail.technoddy@gmail.com>
2024-07-10 23:02:09 -0400

    feedback

--- sql-queries-2/window_functions/top-rows-per-group.sql
+++ sql-queries-2/window_functions/top-rows-per-group.sql
@@ -1,12 +1,12 @@
--- row_number 
+-- with row_number 
 WITH RankedCourses AS ( 
     SELECT *, ROW_NUMBER() OVER (
         PARTITION BY department_id ORDER BY credits DESC
-    ) AS row_num FROM Course 
+    ) AS rank FROM Course 
 ) 
-SELECT * from RankedCourses WHERE row_num <=2;
+SELECT * from RankedCourses WHERE rank <=2;
 
--- rank
+-- with rank
 WITH RankedCourses AS ( 
     SELECT *, RANK() OVER (
         PARTITION BY department_id ORDER BY credits DESC
@@ -14,10 +14,11 @@ WITH RankedCourses AS (
 ) 
 SELECT * from RankedCourses WHERE rank <=2;
 
--- dense_rank
+-- with dense_rank
 WITH RankedCourses AS ( 
     SELECT *, DENSE_RANK() OVER (
         PARTITION BY department_id ORDER BY credits DESC
-    ) AS dense_rank FROM Course 
+    ) AS rank FROM Course 
 ) 
-SELECT * from RankedCourses WHERE dense_rank <=2;
+SELECT * from RankedCourses WHERE rank <=2;
+
