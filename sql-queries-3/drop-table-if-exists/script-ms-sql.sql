e2ace7c4433838bab2fb6529c53887f147333a82
milos-simic <milos-simic@users.noreply.github.com>
2024-07-29 19:17:29 +0200

    Create script-ms-sql.sql

---
+++ sql-queries-3/drop-table-if-exists/script-ms-sql.sql
@@ -0,0 +1,15 @@
+Use University;
+
+SELECT * INTO Active_course
+FROM Course
+WHERE is_active = 'Yes';
+
+SELECT name
+FROM sys.tables;
+
+DROP TABLE IF EXISTS Active_courses;
+
+SELECT name
+FROM sys.tables;
+
+DROP TABLE IF EXISTS Active_courses;
