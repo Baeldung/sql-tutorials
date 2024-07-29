cf588684ba086ba13e71138f32ba83158be1e1a4
Jimmy Azar <76515490+ykxvqze@users.noreply.github.com>
2024-07-29 17:45:19 +0300

    Update mysql-find-in-set-example.sql

--- sql-queries-3/parameterizing-sql-in-clause/mysql-find-in-set-example.sql
+++ sql-queries-3/parameterizing-sql-in-clause/mysql-find-in-set-example.sql
@@ -1,1 +1,6 @@
-SELECT * FROM Student WHERE FIND_IN_SET(id, '1001,1003,1007');
+SET @student_ids = '1001,1003,1007';
+
+PREPARE find_students_by_ids FROM 
+"SELECT * FROM Student WHERE FIND_IN_SET(id, ?)";
+
+EXECUTE find_students_by_ids USING @student_ids;
