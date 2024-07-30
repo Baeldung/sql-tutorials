3dec0d22da67ffce28961c4159d3c0ac10ccdcee
Deepak-Vohra <dvohra10@yahoo.com>
2024-07-30 19:51:46 -0400

    Update savepoint.sql

--- sql-queries-4/savepoint.sql
+++ sql-queries-4/savepoint.sql
@@ -10,14 +10,16 @@ VALUES(7,'Electrical Engineering','EE');
 
 SAVEPOINT sp1; -- Create a savepoint
 
- /* Delete data from Department table */ 
+ /* Delete a row of data from the Department table */ 
 DELETE 
 FROM Department 
 WHERE id=7;
+
+ /* Add a row of data to the Department table */ 
 INSERT 
 INTO Department 
 VALUES(8,'Computer Engineering','CE');
 
-ROLLBACK TO SAVEPOINT sp1; -- Roll back transaction to a savepoint
+ROLLBACK TO SAVEPOINT sp1; -- Rollback transaction to the savepoint
 
-RELEASE SAVEPOINT sp1; -- Release a savepoint
+RELEASE SAVEPOINT sp1; -- Release the savepoint
