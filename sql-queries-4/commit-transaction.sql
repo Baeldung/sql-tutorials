4d7abbc449eb9343253f9670ebd8f9b411c4cfb1
Deepak-Vohra <dvohra10@yahoo.com>
2024-07-30 19:21:42 -0400

    Create commit-transaction.sql

---
+++ sql-queries-4/commit-transaction.sql
@@ -0,0 +1,17 @@
+START TRANSACTION;
+INSERT 
+INTO Department 
+VALUES(6,'Data Science','DS');
+INSERT 
+INTO Department 
+VALUES(7,'Electrical Engineering','EE');
+INSERT 
+INTO Department 
+VALUES(8,'Computer Engineering','CE');
+INSERT 
+INTO Department 
+VALUES(9,'Information Science','IS');
+INSERT 
+INTO Department 
+VALUES(10,'Statistics','S');
+COMMIT;
