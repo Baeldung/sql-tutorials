4360bd0e5ed5d05dcbdd6328564020284a6bb4ff
Deepak-Vohra <dvohra10@yahoo.com>
2024-08-09 11:17:16 -0700

    Update commit.sql

--- sql-queries-4/commit-rollback-savepoint/commit.sql
+++ sql-queries-4/commit-rollback-savepoint/commit.sql
@@ -7,14 +7,5 @@ VALUES(6,'Data Science','DS');
 INSERT 
 INTO Department 
 VALUES(7,'Electrical Engineering','EE');
-INSERT 
-INTO Department 
-VALUES(8,'Computer Engineering','CE');
-INSERT 
-INTO Department 
-VALUES(9,'Information Science','IS');
-INSERT 
-INTO Department 
-VALUES(10,'Statistics','S');
 
 COMMIT; -- Commit the transaction
