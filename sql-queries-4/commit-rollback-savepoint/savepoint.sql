d5888a5465a46c5b3aa2d167505eb8af7399c866
Deepak-Vohra <dvohra10@yahoo.com>
2024-08-09 11:19:49 -0700

    Update savepoint.sql

--- sql-queries-4/commit-rollback-savepoint/savepoint.sql
+++ sql-queries-4/commit-rollback-savepoint/savepoint.sql
@@ -4,21 +4,13 @@ START TRANSACTION; -- Start a new transaction
 INSERT 
 INTO Department 
 VALUES(6,'Data Science','DS');
-INSERT 
-INTO Department 
-VALUES(7,'Electrical Engineering','EE');
 
 SAVEPOINT sp1; -- Create a savepoint
 
  /* Delete a row of data from the Department table */ 
 DELETE 
 FROM Department 
-WHERE id=7;
-
- /* Add a row of data to the Department table */ 
-INSERT 
-INTO Department 
-VALUES(8,'Computer Engineering','CE');
+WHERE id=6;
 
 ROLLBACK TO SAVEPOINT sp1; -- Rollback transaction to the savepoint
 
