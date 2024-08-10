f76d8a408769f1f872b5e72db53268f0694c85bd
Deepak-Vohra <dvohra10@yahoo.com>
2024-08-09 17:26:23 -0700

    Update implicit-commit.sql

--- sql-queries-4/commit-rollback-savepoint/implicit-commit.sql
+++ sql-queries-4/commit-rollback-savepoint/implicit-commit.sql
@@ -1,8 +1,14 @@
 START TRANSACTION; -- Start a new transaction
 
-/* Delete all data from the Department table */ 
+/* Add some data to the Department table */ 
+INSERT 
+INTO Department 
+VALUES(6,'Data Science','DS');
+
+/* Delete the new data from the Department table */ 
 DELETE 
-FROM Department;
+FROM Department
+WHERE id > 5;
 
 CREATE TABLE t(i INTEGER); -- This statement causes an implicit commit
 ROLLBACK; -- This statement has no effect
