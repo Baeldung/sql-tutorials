e1a881176edccabaa5005e918f578b91e0f3309b
oluG4real <michaeloyatoye444@gmail.com>
2024-08-14 11:18:44 +0100

    deleted the syntax queries

--- sql-queries-4/temporary-table-select/using_AS_Syn.sql
+++
@@ -1,5 +0,0 @@
-SELECT * 
-FROM ( 
-       SELECT column1, column2 FROM Existing_table WHERE condition 
-) 
-AS Temp_table;
--- sql-queries-4/temporary-table-select/using_AltSyn_SqlServer.sql
+++
@@ -1,4 +0,0 @@
-SELECT column1, column2 
-INTO #Temporary_table 
-FROM Source_table 
-WHERE condition;
