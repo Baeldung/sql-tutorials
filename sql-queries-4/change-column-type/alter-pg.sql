25d504a317a7938283da33af08848ba8d6b23f81
Yadu Krishnan <yadavan88@gmail.com>
2024-08-18 14:32:10 +0200

    added more queries

--- sql-queries-4/change-column-type/alter-pg.sql
+++ sql-queries-4/change-column-type/alter-pg.sql
@@ -2,3 +2,7 @@ ALTER TABLE course ALTER COLUMN credits TYPE DECIMAL;
 ALTER TABLE course ALTER COLUMN credits TYPE VARCHAR;
 ALTER TABLE course ALTER COLUMN credits TYPE DECIMAL; -- Expected to give error 
 ALTER TABLE course ALTER COLUMN credits TYPE DECIMAL USING credits::INTEGER; -- also resets type
+
+ALTER TABLE course 
+ALTER COLUMN credits TYPE INTEGER
+USING regexp_replace(credits, '\D', '', 'g')::INTEGER; -- extract only numeric from a varchar column
