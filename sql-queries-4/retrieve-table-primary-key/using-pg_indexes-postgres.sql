1909a1766283f149fb5e7a2c86f650abdb9bb1f3
jim-dotcom <ememjimmy.a@gmail.com>
2024-08-26 06:00:36 -0400

    Retrieving Table Primary Key With SQL Queries

---
+++ sql-queries-4/retrieve-table-primary-key/using-pg_indexes-postgres.sql
@@ -0,0 +1,19 @@
+SELECT 
+    indexname AS constraint_name, 
+    tablename AS table_name, 
+    indexdef AS definition 
+FROM 
+    pg_indexes 
+WHERE 
+    tablename = 'student' 
+    AND indexname = 'student_pkey';
+
+SELECT 
+    indexname AS constraint_name, 
+    tablename AS table_name, 
+    indexdef AS definition 
+FROM 
+    pg_indexes 
+WHERE 
+    tablename = 'student' 
+    AND indexname = 'student_pkey';
