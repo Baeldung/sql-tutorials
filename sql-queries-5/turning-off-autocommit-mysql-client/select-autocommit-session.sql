d272d681edfbc5a40ffc367f9bfe4d06050587dc
dvohra09 <dvohra09@yahoo.com>
2024-09-03 15:03:12 -0700

    Update select-autocommit-session.sql

--- sql-queries-5/turning-off-autocommit-mysql-client/select-autocommit-session.sql
+++ sql-queries-5/turning-off-autocommit-mysql-client/select-autocommit-session.sql
@@ -6,3 +6,8 @@ SELECT @@LOCAL.autocommit;
 /* Use one of the SHOW VARIABLES statements. */
 SHOW VARIABLES LIKE 'autocommit';  
 SHOW SESSION VARIABLES LIKE 'autocommit'
+
+/* Query the perfrmance_schema */
+SELECT * 
+FROM performance_schema.session_variables
+WHERE VARIABLE_NAME = 'autocommit';
