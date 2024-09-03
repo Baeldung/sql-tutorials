b25fcd4460150ef97a24d6524a88d3d3e2d0cdbf
dvohra09 <dvohra09@yahoo.com>
2024-09-03 15:01:49 -0700

    Create select-autocommit-session.sql

---
+++ sql-queries-5/turning-off-autocommit-mysql-client/select-autocommit-session.sql
@@ -0,0 +1,8 @@
+/* Use one of the SELECT statements. */
+SELECT @@autocommit;
+SELECT @@SESSION.autocommit;
+SELECT @@LOCAL.autocommit;
+
+/* Use one of the SHOW VARIABLES statements. */
+SHOW VARIABLES LIKE 'autocommit';  
+SHOW SESSION VARIABLES LIKE 'autocommit'
