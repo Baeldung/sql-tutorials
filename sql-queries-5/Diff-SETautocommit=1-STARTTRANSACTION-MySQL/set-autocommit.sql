28dbd0f87cc045514ed509eb06509b7de8762f4a
Deepak-Vohra <dvohra10@yahoo.com>
2024-09-04 09:10:01 -0700

    Create set-autocommit.sql

---
+++ sql-queries-5/Diff-SETautocommit=1-STARTTRANSACTION-MySQL/set-autocommit.sql
@@ -0,0 +1,13 @@
+SET autocommit=1; --Enables autocommit mode
+
+/*These are equivalent*/
+SET autocommit = DEFAULT;
+
+SET autocommit='ON';
+SET @@autocommit = 'ON';
+
+SET SESSION autocommit = 'ON';
+SET @@SESSION.autocommit = 'ON';
+
+SET LOCAL autocommit = 'ON';
+SET @@LOCAL.autocommit = 'ON';
