2366ecc34a1a8459087cd575867e61fc99ba7257
Deepak-Vohra <dvohra10@yahoo.com>
2024-09-06 10:54:58 -0700

    Update autocommit-enabled.sql

--- sql-queries-5/Diff-SETautocommit=1-STARTTRANSACTION-MySQL/autocommit-enabled.sql
+++ sql-queries-5/Diff-SETautocommit=1-STARTTRANSACTION-MySQL/autocommit-enabled.sql
@@ -1,7 +1,13 @@
-/*Each of these runs in a separate transaction*/
-SELECT SELECT * FROM DEPARTMENT;
-INSERT INTO Department VALUES(6,'Data Science','DS');
-INSERT INTO Department VALUES(7,'Computer Engineering','CE');
-UPDATE Department SET name = 'Math' WHERE id=5;
+SET autocommit=1; --Turns on automatic transaction mode
 
-SELECT 1; --This statement doesn't run in a transaction
+CREATE TABLE Department
+(
+    id INT PRIMARY KEY NOT Null,
+    name VARCHAR (50),
+    code VARCHAR (4),
+    UNIQUE (id)
+); -- Runs in a separate transaction
+
+SELECT SELECT * FROM DEPARTMENT; --Runs in a separate transaction
+
+INSERT INTO Department VALUES(6,'Data Science','DS'); --Runs in a separate transaction
