174e8b73c751ccc8cabbeac25cf0b484f2e006dc
Deepak-Vohra <dvohra10@yahoo.com>
2024-09-06 10:56:17 -0700

    Update start-transaction.sql

--- sql-queries-5/Diff-SETautocommit=1-STARTTRANSACTION-MySQL/start-transaction.sql
+++ sql-queries-5/Diff-SETautocommit=1-STARTTRANSACTION-MySQL/start-transaction.sql
@@ -1,9 +1,9 @@
-START TRANSACTION; --Starts a transaction
+START TRANSACTION; --Starts a single transaction
 
-/* SQL Statements run in transaction
+/* SQL Statements*/
 SELECT SELECT * FROM DEPARTMENT;
 INSERT INTO Department VALUES(6,'Data Science','DS');
 INSERT INTO Department VALUES(7,'Computer Engineering','CE');
-UPDATE Department SET name = 'Math' WHERE id=5; */
+UPDATE Department SET name = 'Math' WHERE id=5;  
 
-COMMIT; -- or ROLLBACK; to commit or rollback transaction
+COMMIT; -- or ROLLBACK ends the single transaction
