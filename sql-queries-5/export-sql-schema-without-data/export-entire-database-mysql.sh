3b8256ca8f64f4225c69231bb82845fa8a62a577
jim-dotcom <ememjimmy.a@gmail.com>
2024-09-10 02:15:59 -0400

    How to Export SQL Schema Without Data

--- sql-queries-5/export-sql-schema-without-data/export-entire-database-mysql.sh
+++ sql-queries-5/export-sql-schema-without-data/export-entire-database-mysql.sh
@@ -1,6 +1,6 @@
-$ mysqldump -u user -p -h 127.0.0.1 --no-data University > university_schema.sql
+mysqldump -u user -p -h 127.0.0.1 --no-data University > university_schema.sql
 
-$ ls -lh
+ls -lh
 
-$ cat university_schema.sql
+cat university_schema.sql
 
