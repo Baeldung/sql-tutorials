3e59f13113f96d8ad63a5b3bb52ee01cab65f888
Haroon Javed <141476933+haroon445@users.noreply.github.com>
2024-09-11 11:17:49 +0500

    Update over-clause-with-rows.sql

--- sql-queries-5/sql-over-clause/over-clause-with-rows.sql
+++ sql-queries-5/sql-over-clause/over-clause-with-rows.sql
@@ -1,7 +1,5 @@
 SELECT id, enrollment_date,
-ROW_NUMBER() OVER (ORDER BY enrollment_date) AS EnrollmentOrder,
-COUNT(*) OVER (PARTITION BY YEAR(enrollment_date)) AS StudentsPerYear,
 AVG(gpa) OVER (ORDER BY enrollment_date ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) AS Avg3GPAs,
-LEAD(name, 1, 'No next student') OVER (ORDER BY id) AS NextStudent
+AVG(gpa) OVER (ORDER BY YEAR(enrollment_date) RANGE BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS AvgGPAYearOverYear
 FROM Student
 ORDER BY enrollment_date, id;
