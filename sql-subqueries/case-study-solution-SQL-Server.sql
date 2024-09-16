07117b02e89ce138a6375fae079a90c4d6f0d420
Latsan <78388641+Latsan@users.noreply.github.com>
2024-09-16 21:45:01 +0100

    Delete sql-subqueries/case-study-solution-SQL-Server.sql

--- sql-subqueries/case-study-solution-SQL-Server.sql
+++
@@ -1,9 +0,0 @@
-SELECT s.*, e.course_name, e.grade, e.exam_date
-FROM Student s
-OUTER APPLY (
-    SELECT TOP 1 c.name AS course_name, e.grade, e.exam_date
-    FROM Exam e
-    JOIN Course c ON e.course_id = c.id
-    WHERE e.student_id = s.id
-    ORDER BY e.exam_date DESC
-) e;
