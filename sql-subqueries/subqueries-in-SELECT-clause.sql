dab1963edd2fcc5b4acd78d40710f7e6c272003a
Latsan <78388641+Latsan@users.noreply.github.com>
2024-09-26 09:09:19 +0100

    Delete sql-subqueries/subqueries-in-SELECT-clause.sql

--- sql-subqueries/subqueries-in-SELECT-clause.sql
+++
@@ -1,6 +0,0 @@
-SELECT s.name,
-       (SELECT e.exam_date
-        FROM Exam e
-        WHERE e.student_id = s.id 
-        AND e.exam_date >= ALL (SELECT exam_date FROM Exam WHERE student_id = s.id)) AS last_exam_date
-FROM Student s;
