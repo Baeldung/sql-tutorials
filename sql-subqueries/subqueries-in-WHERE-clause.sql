e29a3ed1df51ca55a16faeac5eb4a6c5f3ac9457
Latsan <78388641+Latsan@users.noreply.github.com>
2024-09-26 09:09:35 +0100

    Delete sql-subqueries/subqueries-in-WHERE-clause.sql

--- sql-subqueries/subqueries-in-WHERE-clause.sql
+++
@@ -1,8 +0,0 @@
-SELECT s.name
-FROM Student s
-WHERE EXISTS (
-    SELECT 1
-    FROM Exam e
-    WHERE e.student_id = s.id 
-    AND e.exam_date >= ALL (SELECT exam_date FROM Exam WHERE student_id = s.id)
-);
