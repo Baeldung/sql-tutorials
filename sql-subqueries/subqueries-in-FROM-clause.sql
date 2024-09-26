c7aa8ae37f2dc36e02b85606a1b23841a87ad9fe
Latsan <78388641+Latsan@users.noreply.github.com>
2024-09-26 09:08:58 +0100

    Delete sql-subqueries/subqueries-in-FROM-clause.sql

--- sql-subqueries/subqueries-in-FROM-clause.sql
+++
@@ -1,8 +0,0 @@
-SELECT s.name, latest_exam.course_name
-FROM Student s
-JOIN (
-    SELECT e.student_id, c.name AS course_name
-    FROM Exam e
-    JOIN Course c ON e.course_id = c.id
-    WHERE e.exam_date >= ALL (SELECT exam_date FROM Exam WHERE student_id = e.student_id)
-) latest_exam ON s.id = latest_exam.student_id;
