a140743f06c4420f98e8824d40230a97ad56a308
Peace0095 <104388733+Peace0095@users.noreply.github.com>
2024-09-27 21:05:10 +0100

    Add these file for this tutorial

---
+++ sql-queries-6/Extracting-the-First-N-Characters-of-a-Value-in-SQL/Extracting_the_First_N_Characters_Based_on_Conditions.sql
@@ -0,0 +1,1 @@
+SELECT id, LEFT(name, 3) AS name_start, department_id FROM Faculty WHERE department_id = (SELECT id FROM Department WHERE name = 'Computer Science') LIMIT 5;
---
+++ sql-queries-6/Extracting-the-First-N-Characters-of-a-Value-in-SQL/Using_LEFT().sql
@@ -0,0 +1,1 @@
+SELECT id, LEFT(textbook, 10) AS textbook_start FROM Course WHERE textbook IS NOT NULL LIMIT 5;
