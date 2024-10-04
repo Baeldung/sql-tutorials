08249063bc04d7d9448f1712983be5d8706d01c6
Peace0095 <104388733+Peace0095@users.noreply.github.com>
2024-10-04 15:48:26 +0100

    Using_LEFT().sql

--- sql-queries-6/Extracting-the-First-N-Characters-of-a-Value-in-SQL/Using_LEFT().sql
+++ sql-queries-6/Extracting-the-First-N-Characters-of-a-Value-in-SQL/Using_LEFT().sql
@@ -1,1 +1,6 @@
-SELECT id, LEFT(textbook, 10) AS textbook_start FROM Course WHERE textbook IS NOT NULL LIMIT 5;
+SELECT
+    id,
+    LEFT(textbook, 10) AS textbook_start
+FROM Course
+WHERE textbook IS NOT NULL
+LIMIT 5;
