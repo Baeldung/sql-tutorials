0aea2054e3e07b6d343cf3f220668a917d4602d5
Sidrah Abdullah <58777694+degr8sid-code@users.noreply.github.com>
2024-10-04 21:41:57 -0600

    Update running-total.sql

--- sql-queries/calculate-running-total/running-total.sql
+++ sql-queries/calculate-running-total/running-total.sql
@@ -2,11 +2,9 @@ SELECT id, scores, SUM(scores)
 OVER (ORDER BY id) AS RunningTotal
 FROM Exam;
 
-
 SELECT id, scores, (SELECT SUM(scores)
                     FROM Exam e2 WHERE e2.id <= e1.id) AS RunningTotal
 FROM Exam e1 ORDER BY id;
-
 
 SELECT s1.id, s1.scores, SUM(s2.scores) AS RunningTotal
 FROM Exam s1
