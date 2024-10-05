faf24d247769bdc4477b9a283653a850792616de
Peace0095 <104388733+Peace0095@users.noreply.github.com>
2024-10-05 22:17:11 +0100

    Extracting_the_First_N_Characters_Based_on_Conditions.sql

--- sql-queries-6/Extracting-the-First-N-Characters-of-a-Value-in-SQL/Extracting_the_First_N_Characters_Based_on_Conditions.sql
+++ sql-queries-6/Extracting-the-First-N-Characters-of-a-Value-in-SQL/Extracting_the_First_N_Characters_Based_on_Conditions.sql
@@ -1,11 +1,11 @@
 SELECT
-    id,
-    LEFT(name, 3) AS name_start,
-    department_id
+id,
+LEFT(name, 3) AS name_start,
+department_id
 FROM Faculty
 WHERE department_id = (
-    SELECT id
-    FROM Department
-    WHERE name = 'Computer Science'
+SELECT id
+FROM Department
+WHERE name = 'Computer Science'
 )
 LIMIT 5;
