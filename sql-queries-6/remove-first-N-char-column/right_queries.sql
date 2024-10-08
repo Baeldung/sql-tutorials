10ae052e1093df3fb6ff0115f48088c21c8415f4
Hrishikesh Dhondge <hbdhondge@gmail.com>
2024-10-08 06:11:39 +0200

    Update sql-queries-6/remove-first-N-char-column/right_queries.sql
    Co-authored-by: Liam Williams <liam.williams@zoho.com>

--- sql-queries-6/remove-first-N-char-column/right_queries.sql
+++ sql-queries-6/remove-first-N-char-column/right_queries.sql
@@ -1,3 +1,3 @@
 SELECT RIGHT('websitebaeldung', 8);
-SELECT RIGHT(id_copy, LENGTH(id_copy) - 2) from Course;
+SELECT RIGHT(id_copy, LENGTH(id_copy) - 2) FROM Course;
 UPDATE Course SET id_copy = RIGHT(id_copy, LENGTH(id_copy) - 2);
