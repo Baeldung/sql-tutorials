01c2b52519a61f98b6519971c15dd7bab0e67c23
thescholars14 <thescholars14@gmail.com>
2024-10-09 21:41:34 +0500

    Create add-foreign-key-postgresql.sql

--- sql-queries-6/add-foreign-key-existing-table/add-foreign-key-postgresql.sql
+++ sql-queries-6/add-foreign-key-existing-table/add-foreign-key-postgresql.sql
@@ -1,6 +1,9 @@
+--adding a new column
+ALTER TABLE student
+ADD COLUMN department_id INT;
+
 --adding a foreign key constraint to an existing table
 ALTER TABLE student
-ADD COLUMN department_id INT,
 ADD CONSTRAINT fk_student_department
 FOREIGN KEY (department_id) REFERENCES department(id);
 
