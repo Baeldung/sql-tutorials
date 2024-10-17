530c653e3aaa15f1433823995237218dcf2aae74
thescholars14 <thescholars14@gmail.com>
2024-10-17 16:26:37 +0500

    Update add-foreign-key-mysql.sql

--- sql-queries-6/add-foreign-key-existing-table/add-foreign-key-mysql.sql
+++ sql-queries-6/add-foreign-key-existing-table/add-foreign-key-mysql.sql
@@ -1,17 +1,17 @@
 --adding a new column for foreign key
-ALTER TABLE student
+ALTER TABLE Student
 ADD COLUMN department_id INT;
 
 -- adding a foreign key to an existing table
-ALTER TABLE student
+ALTER TABLE Student
 ADD CONSTRAINT fk_student_department
-FOREIGN KEY (department_id) REFERENCES department(id);
+FOREIGN KEY (department_id) REFERENCES Department(id);
 
 --adding unique constraints in the department table
-ALTER TABLE department
+ALTER TABLE Department
 ADD CONSTRAINT uq_department UNIQUE (id, name);
 
 --adding a foreign key to multiple columns
-ALTER TABLE course
+ALTER TABLE Course
 ADD CONSTRAINT fk_course_department FOREIGN KEY (department_id, name)
-REFERENCES department (id, name);
+REFERENCES Department (id, name);
