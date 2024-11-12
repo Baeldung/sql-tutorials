d05adc965a094e1d9a033dd0f515d51aa3ec0704
Sidrah Abdullah <58777694+degr8sid-code@users.noreply.github.com>
2024-11-11 17:01:40 -0700

    Update rolling-average-mysql.sql

--- sql-queries-7/rolling-average/rolling-average-mysql.sql
+++ sql-queries-7/rolling-average/rolling-average-mysql.sql
@@ -1,6 +1,6 @@
-CREATE VIEW v_DailyEnrollmentWithAvg
-  WITH SCHEMABINDING AS
-  SELECT date, total_registrations, AVG(total_registrations) OVER ( ORDER BY date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW )
+CREATE EVENT update_rolling_avg
+  ON SCHEDULE EVERY 1 DAY
+  DO REPLACE INTO DailyEnrollmentRollingAvg
+  SELECT date, total_registrations, AVG(total_registrations) OVER ( ORDER BY date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW ) 
   AS rolling_avg_7_days
-  FROM DailyEnrollment; 
-GO CREATE UNIQUE CLUSTERED INDEX idx_DailyEnrollmentWithAvg ON v_DailyEnrollmentWithAvg (date);
+  FROM DailyEnrollment;
