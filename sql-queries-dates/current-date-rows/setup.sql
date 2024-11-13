31498ec24453e034846dfc57bb7b5d6d0f6761be
Yadukrishnan <yadavan88@gmail.com>
2024-11-13 21:24:53 +0100

    Queries to get all ros for the current date on a timestamp column

---
+++ sql-queries-dates/current-date-rows/setup.sql
@@ -0,0 +1,3 @@
+UPDATE Registration SET reg_datetime = current_timestamp WHERE id = 1;
+UPDATE Registration SET reg_datetime = current_timestamp WHERE id = 101;
+UPDATE Registration SET reg_datetime = current_timestamp WHERE id = 201;
