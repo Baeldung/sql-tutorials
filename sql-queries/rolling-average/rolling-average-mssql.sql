04f2386291c58ad113a3936b3b36ad271c88a9a2
Sidrah Abdullah <58777694+degr8sid-code@users.noreply.github.com>
2024-11-14 15:59:47 -0700

    Delete sql-queries/rolling-average directory

--- sql-queries/rolling-average/rolling-average-mssql.sql
+++
@@ -1,6 +0,0 @@
-CREATE VIEW v_DailyEnrollmentWithAvg
-  WITH SCHEMABINDING AS
-  SELECT date, total_registrations, AVG(total_registrations) OVER ( ORDER BY date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW )
-  AS rolling_avg_7_days
-  FROM DailyEnrollment; 
-GO CREATE UNIQUE CLUSTERED INDEX idx_DailyEnrollmentWithAvg ON v_DailyEnrollmentWithAvg (date);
--- sql-queries/rolling-average/rolling-average.sql
+++
@@ -1,4 +0,0 @@
-SELECT date, total_registrations, AVG(total_registrations)
-  OVER ( ORDER BY date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW ) AS rolling_avg_7_days
-  FROM DailyEnrollment
-  ORDER BY date;
