55ff6d6a153d4d357637ed2a611226db6006dfa9
Yadu Krishnan <yadavan88@gmail.com>
2024-11-18 21:56:00 +0100

    added reset script

--- sql-queries-dates/current-date-rows/setup.sql
+++ sql-queries-dates/current-date-rows/setup.sql
@@ -1,3 +1,8 @@
 UPDATE Registration SET reg_datetime = current_timestamp WHERE id = 1;
 UPDATE Registration SET reg_datetime = current_timestamp WHERE id = 101;
 UPDATE Registration SET reg_datetime = current_timestamp WHERE id = 201;
+
+-- Reset the rows with original value
+UPDATE Registration SET reg_datetime = '2022-01-11 12:45:56' WHERE id = 1;
+UPDATE Registration SET reg_datetime = '2022-08-05 12:45:56' WHERE id = 101;
+UPDATE Registration SET reg_datetime = '2023-01-11 12:45:56' WHERE id = 201;
