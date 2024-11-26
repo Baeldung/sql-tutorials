04c3c1c4f4cbaff9a4a6d3722b1eb68dba7a42c5
Latsan <78388641+Latsan@users.noreply.github.com>
2024-11-26 16:31:23 +0000

    Update implementing_batch_delete_alternative.sql

--- sql-queries-8/reasons_the_DELETE_statement_in_SQL_can_be_very_slow/implementing_batch_delete_alternative.sql
+++ sql-queries-8/reasons_the_DELETE_statement_in_SQL_can_be_very_slow/implementing_batch_delete_alternative.sql
@@ -1,8 +1,21 @@
-WITH batch AS (
-    SELECT id 
-    FROM Registration 
-    WHERE year < 2022
-    LIMIT 1000
-)
-DELETE FROM Registration 
-WHERE id IN (SELECT id FROM batch);
+DELIMITER //
+
+CREATE PROCEDURE BatchDelete()
+BEGIN
+    DECLARE rows_affected INT DEFAULT 1;
+    DECLARE batch_size INT DEFAULT 1000;
+    
+    WHILE rows_affected > 0 DO
+        DELETE FROM Registration 
+        WHERE year < 2022 
+        LIMIT batch_size;
+        
+        SET rows_affected = ROW_COUNT();
+    END WHILE;
+END //
+
+DELIMITER ;
+
+CALL BatchDelete();
+
+DROP PROCEDURE BatchDelete;
