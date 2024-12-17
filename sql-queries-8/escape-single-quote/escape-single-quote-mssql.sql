e7376fd8b616ea38dc9698cf1c4e92d6ba521139
sriramramanujamcw <sriram.ramanujam.cw@gmail.com>
2024-12-17 02:40:54 -0800

    Create escape-single-quote-mssql.sql

---
+++ sql-queries-8/escape-single-quote/escape-single-quote-mssql.sql
@@ -0,0 +1,5 @@
+INSERT INTO Student (id, name, national_id, birth_date, enrollment_date, graduation_date, gpa) 
+  VALUES (1001, 'John O' + NCHAR(39) + 'Liu', 123345566, '2001-04-05', '2020-01-15', '2024-06-15', 4);
+
+SELECT * FROM Student 
+  WHERE name = 'John O' + NCHAR(39) + 'Liu';
