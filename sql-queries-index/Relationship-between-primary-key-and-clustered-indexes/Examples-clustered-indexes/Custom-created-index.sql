7b64c31b0a988f3f934184bd1c74aa9349df75e6
Hannah Igboke <116895464+HannahIgboke@users.noreply.github.com>
2024-12-20 13:20:04 +0100

    Update Custom-created-index.sql

--- sql-queries-index/Relationship-between-primary-key-and-clustered-indexes/Examples-clustered-indexes/Custom-created-index.sql
+++ sql-queries-index/Relationship-between-primary-key-and-clustered-indexes/Examples-clustered-indexes/Custom-created-index.sql
@@ -1,5 +1,8 @@
-CREATE TABLE Orders ( 
-  OrderID INT PRIMARY KEY, 
-  OrderDate DATETIME, 
-  CustomerID INT); 
-CREATE CLUSTERED INDEX IX_OrderDate ON Orders (OrderDate);
+CREATE TABLE Library ( 
+  book_id INT PRIMARY KEY, 
+  book_title VARCHAR(255), 
+  author VARCHAR(255), 
+  isbn VARCHAR(20), 
+  available_copies INT, 
+  borrowed_date DATE); 
+CREATE CLUSTERED INDEX IX_borrowed_date ON Library (borrowed_date);
