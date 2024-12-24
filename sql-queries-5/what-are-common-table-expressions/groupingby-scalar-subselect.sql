8e586d90c43b77f4006ca30bfcb28dd32d922d19
daKeshra <keshradamora@gmail.com>
2024-12-24 15:32:19 +0100

    modified recursive cte query

--- sql-queries-5/what-are-common-table-expressions/groupingby-scalar-subselect.sql
+++ sql-queries-5/what-are-common-table-expressions/groupingby-scalar-subselect.sql
@@ -4,9 +4,9 @@ WITH AgeCategories AS (
             s.name AS student_name, 
             TIMESTAMPDIFF(YEAR, s.birth_date, s.enrollment_date) AS age_at_enrollment, 
             CASE 
-                WHEN TIMESTAMPDIFF(YEAR, s.birth_date, s.enrollment_date) < 20 THEN 'Under 20' 
-                WHEN TIMESTAMPDIFF(YEAR, s.birth_date, s.enrollment_date) BETWEEN 20 AND 30 THEN '20-30' 
-                ELSE 'Over 30' 
+                WHEN TIMESTAMPDIFF(YEAR, s.birth_date, s.enrollment_date) < 18 THEN 'Under 18' 
+                WHEN TIMESTAMPDIFF(YEAR, s.birth_date, s.enrollment_date) = 18 THEN '18 Years' 
+                ELSE 'Above 18' 
             END AS age_category 
     FROM Student AS s 
     ) 
--- sql-queries-5/what-are-common-table-expressions/recursive-cte-queries.sql
+++ sql-queries-5/what-are-common-table-expressions/recursive-cte-queries.sql
@@ -1,13 +1,36 @@
 -- recursive CTE query
-WITH RECURSIVE StudentCourse AS ( 
-    SELECT student_id, course_id, 1 AS level 
-    FROM Exam 
-    WHERE course_id = 'CS111' 
+WITH RECURSIVE SemesterProgression AS ( 
+    SELECT e.student_id, 
+           e.semester AS current_semester, 
+           e.exam_date AS current_exam_date, 
+           e.course_id, c.name AS course_name, 
+           e.grade, e.semester AS semester_chain 
+    FROM 
+        Exam AS e 
+        JOIN Course AS c 
+            ON e.course_id = c.id 
+        WHERE e.student_id = 1001 AND e.semester = (SELECT MIN(semester) FROM exam WHERE student_id = 1001) 
+    
     UNION ALL 
-        SELECT e.student_id, e.course_id, sh.level + 1 
-        FROM Exam AS e 
-        INNER JOIN StudentCourse AS sc 
-            ON e.student_id = sh.course_id 
+    
+    SELECT e.student_id, 
+           e.semester AS current_semester, 
+           e.exam_date AS current_exam_date, 
+           e.course_id, c.name AS course_name, 
+           e.grade, 
+           CONCAT(sp.semester_chain, ' -> ', e.semester) AS semester_chain 
+    FROM Exam AS e 
+    JOIN SemesterProgression AS sp 
+        ON e.student_id = sp.student_id 
+    JOIN Course AS c 
+        ON e.course_id = c.id 
+    WHERE e.semester > sp.current_semester 
     ) 
-    SELECT student_id, course_id, level 
-    FROM StudentCourse;
+    -- query to display the semester progression 
+    SELECT student_id, 
+            current_semester, 
+            current_exam_date, 
+            course_name, grade, 
+            semester_chain 
+    FROM SemesterProgression 
+    ORDER BY current_exam_date;
