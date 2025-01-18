55d42eeb37fd1ec2f6a17321e47c789dc26531f2
Jimmy Azar <76515490+ykxvqze@users.noreply.github.com>
2025-01-18 11:15:25 +0200

    Update full-text-search-sqlserver.sql

--- sql-queries-9/keyword-search-sql/full-text-search-sqlserver.sql
+++ sql-queries-9/keyword-search-sql/full-text-search-sqlserver.sql
@@ -1,5 +1,5 @@
 CREATE FULLTEXT CATALOG ftCatalog AS DEFAULT; 
-CREATE FULLTEXT INDEX ON Course(name, textbook)
+CREATE FULLTEXT INDEX ON Course(name, textbook) KEY INDEX PK_Course;
 
 SELECT id, name, textbook, credits 
 FROM Course 
