1cb8d6e6f8aa1d0477051f4042e191bbc6a052ec
Hannah Igboke <116895464+HannahIgboke@users.noreply.github.com>
2025-02-02 12:48:09 +0100

    Delete sql-generation/Generate SQL Insert Script directory

--- sql-generation/Generate SQL Insert Script/new data insert.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO Student (id, name, national_id, birth_date, enrollment_date, graduation_date, gpa) 
-VALUES (7, ‘George Bailey’, 3125372499, ‘1997-08-14’, ‘2017-09-01’, ‘2021-06-15’, 3.41);
