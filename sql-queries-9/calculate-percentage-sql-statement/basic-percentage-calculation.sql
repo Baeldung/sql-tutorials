8eb73cecce51c1d06e468c2eb86c419174b53abe
Jimmy Azar <76515490+ykxvqze@users.noreply.github.com>
2025-02-07 11:35:04 +0200

    Update basic-percentage-calculation.sql

--- sql-queries-9/calculate-percentage-sql-statement/basic-percentage-calculation.sql
+++ sql-queries-9/calculate-percentage-sql-statement/basic-percentage-calculation.sql
@@ -2,22 +2,22 @@ SELECT
     name,
     gpa,
     (gpa / (SELECT SUM(gpa) FROM Student)) * 100 AS gpa_percentage
-FROM student;
+FROM Student;
 
 SELECT
     name,
     gpa,
     ROUND(((gpa::numeric) / (SELECT SUM(gpa)::numeric FROM Student)) * 100.00, 2) AS gpa_percentage
-FROM student;
+FROM Student;
 
 SELECT 
     name, 
     gpa, 
     (CAST(gpa AS FLOAT) / (SELECT SUM(CAST(gpa AS FLOAT)) FROM Student)) * 100 AS gpa_percentage
-FROM student;
+FROM Student;
 
 SELECT 
     name, 
     gpa, 
     ROUND((CAST(gpa AS FLOAT) / (SELECT SUM(CAST(gpa AS FLOAT)) FROM Student)) * 100, 2) AS gpa_percentage
-FROM student;
+FROM Student;
