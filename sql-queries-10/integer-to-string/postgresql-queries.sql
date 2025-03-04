15341c3b4cc9e9993dc1bce752d67cbaaafc1409
sukstechs2311 <sukstechs2311@gmail.com>
2025-03-04 05:51:50 -0800

    Create postgresql-queries.sql

---
+++ sql-queries-10/integer-to-string/postgresql-queries.sql
@@ -0,0 +1,10 @@
+SELECT * 
+FROM Faculty;
+
+SELECT CONCAT(CAST(SPLIT_PART(name, ' ', 1) AS text), '_', CAST(national_id AS text), '@baeldung.com') AS allocated_email_id
+FROM Faculty 
+LIMIT 2;
+
+SELECT CONCAT(SPLIT_PART(name, ' ', 1), '_', national_id, '@baeldung.com') AS allocated_email_id
+FROM Faculty
+LIMIT 2;
