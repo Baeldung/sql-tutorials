94ef2874987cd6ccdb8649d8cc73a919442524fc
Deepak-Vohra <dvohra10@yahoo.com>
2025-04-06 14:18:19 -0700

    Create optimize-all-tables-with-mysqlcheck.cmd

---
+++ sql-queries-10/optimize-all-tables-mysql/optimize-all-tables-with-mysqlcheck.bash
@@ -0,0 +1,7 @@
+$ mysqlcheck -u root -o university
+
+$ mysqlcheck -u root -o university_myisam
+
+$ mysqlcheck -u root -o --databases university  university_myisam
+
+$ mysqlcheck -u root -o --all-databases
