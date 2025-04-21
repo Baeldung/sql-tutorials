6181690f448a779692e798a69cb479a90ef97b75
Jimmy Azar <76515490+ykxvqze@users.noreply.github.com>
2025-04-21 10:09:55 +0300

    Update mysql-check.sh

--- sql-queries-10/checking-database-connectivity/mysql-check.sh
+++ sql-queries-10/checking-database-connectivity/mysql-check.sh
@@ -8,7 +8,7 @@ source mysql.env
 echo "Testing MySQL connectivity..."
 
 # Test MySQL connectivity and check for 'Department' table
-mysql -h "$MYSQL_HOST" -P "$MYSQL_PORT" -u "$MYSQL_USER" -p "$MYSQL_PASS" -e "USE $MYSQL_DB; SHOW TABLES LIKE 'Department';" >/dev/null 2>&1
+mysql -h "$MYSQL_HOST" -P "$MYSQL_PORT" -u "$MYSQL_USER" -p"$MYSQL_PASS" -e "USE $MYSQL_DB; SHOW TABLES LIKE 'Department';" >/dev/null 2>&1
 
 if [ $? -eq 0 ]; then
   echo "MySQL: Connection successful and department table exists."
