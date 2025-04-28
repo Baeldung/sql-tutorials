47a05478618bac482756e207563c791619d7d43d
Hannah Igboke <116895464+HannahIgboke@users.noreply.github.com>
2025-04-28 11:50:19 +0100

    Update rebuild_all_index_in_db.sql

--- sql-queries-index/index-rebuild/rebuild_all_index_in_db.sql
+++ sql-queries-index/index-rebuild/rebuild_all_index_in_db.sql
@@ -1,24 +1,33 @@
-USE University; 
-GO 
+USE University;
+GO
 
-DECLARE @TableName NVARCHAR(255); 
-DECLARE @SQL NVARCHAR(MAX); 
+DECLARE @TableName NVARCHAR(255);
+DECLARE @IndexName NVARCHAR(255);
+DECLARE @SQL NVARCHAR(MAX);
 
-DECLARE table_cursor CURSOR FOR 
-SELECT QUOTENAME(SCHEMA_NAME(t.schema_id)) + '.' + QUOTENAME(t.name) 
-FROM sys.tables t 
-WHERE t.is_ms_shipped = 0; 
+DECLARE frag_cursor CURSOR FOR
+SELECT 
+    QUOTENAME(SCHEMA_NAME(t.schema_id)) + '.' + QUOTENAME(t.name),
+    QUOTENAME(i.name)
+FROM 
+    sys.dm_db_index_physical_stats(DB_ID(), NULL, NULL, NULL, 'LIMITED') AS ps
+JOIN sys.indexes i ON ps.object_id = i.object_id AND ps.index_id = i.index_id
+JOIN sys.tables t ON i.object_id = t.object_id
+WHERE 
+    ps.avg_fragmentation_in_percent > 30
+    AND i.type_desc IN ('CLUSTERED', 'NONCLUSTERED');
 
-OPEN table_cursor; 
-FETCH NEXT FROM table_cursor INTO @TableName; 
+OPEN frag_cursor;
+FETCH NEXT FROM frag_cursor INTO @TableName, @IndexName;
 
-WHILE @@FETCH_STATUS = 0 
-BEGIN 
-  SET @SQL = 'ALTER INDEX ALL ON ' + @TableName + ' REBUILD WITH (FILLFACTOR = 90, SORT_IN_TEMPDB = ON);'; 
-  PRINT 'Rebuilding indexes on: ' + @TableName; 
-  EXEC sp_executesql @SQL; 
-  FETCH NEXT FROM table_cursor INTO @TableName; 
-END; 
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    SET @SQL = 'ALTER INDEX ' + @IndexName + ' ON ' + @TableName + ' REBUILD WITH (FILLFACTOR = 90, SORT_IN_TEMPDB = ON);';
+    PRINT 'Rebuilding index: ' + @IndexName + ' on table: ' + @TableName;
+    EXEC sp_executesql @SQL;
 
-CLOSE table_cursor; 
-DEALLOCATE table_cursor;
+    FETCH NEXT FROM frag_cursor INTO @TableName, @IndexName;
+END;
+
+CLOSE frag_cursor;
+DEALLOCATE frag_cursor;
