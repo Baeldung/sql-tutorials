4303b1e13c0463b2e661ad6e8da51af4d648aa0f
Loredana Crusoveanu <lore.crusoveanu@gmail.com>
2025-04-30 12:09:45 +0300

    clean duplicate code and move it alll to data-manipulation

--- sql-queries-7/remove-duplicate-row-in-sql/using-rownumber-with-partitionby.sql
+++
@@ -1,7 +0,0 @@
-SELECT id, semester, year, course_id, student_id,
-    ROW_NUMBER() OVER (
-        PARTITION BY semester, year, course_id, student_id
-        ORDER BY id
-    ) AS rn
-FROM
-    Registration;
