dded59d6c2ba430baf9427011da737411cda8958
Jimmy Azar <76515490+ykxvqze@users.noreply.github.com>
2025-06-10 20:42:33 +0300

    Update single-row-from-join.sql

--- sql-queries-11/single-row-from-join/single-row-from-join.sql
+++ sql-queries-11/single-row-from-join/single-row-from-join.sql
@@ -1,11 +1,14 @@
-SELECT countries.country_id, country_name, city_id, city_name, population FROM countries
-	 JOIN cities ON countries.country_id = cities.country_id;
+SELECT countries.country_id, country_name, city_id, city_name, population
+FROM countries
+JOIN cities ON countries.country_id = cities.country_id;
 
 SELECT DISTINCT ON(countries.country_id) 
-	countries.country_id, country_name, city_id, city_name, population FROM countries
-	 JOIN cities ON countries.country_id = cities.country_id;
+    countries.country_id, country_name, city_id, city_name, population
+FROM countries
+JOIN cities ON countries.country_id = cities.country_id;
 
 SELECT DISTINCT ON(countries.country_id) 
-	countries.country_id, country_name, city_id, city_name, population FROM countries
-	 JOIN cities ON countries.country_id = cities.country_id
-	ORDER BY countries.country_id, population DESC;
+    countries.country_id, country_name, city_id, city_name, population
+FROM countries
+JOIN cities ON countries.country_id = cities.country_id
+ORDER BY countries.country_id, population DESC;
