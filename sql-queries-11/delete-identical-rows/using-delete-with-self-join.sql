47a3598494d7e60740a8bd2236bbdd851fe26ba9
Kapil Khandelwal <kapilbk1996@gmail.com>
2025-06-21 13:19:50 +0530

    Add separate query for MS SQL

--- sql-queries-11/delete-identical-rows/using-delete-with-self-join.sql
+++ sql-queries-11/delete-identical-rows/using-delete-with-self-join.sql
@@ -7,7 +7,7 @@ WHERE
     DR1.Value3 = DR2.Value3 AND
     DR1.RecordID > DR2.RecordID;
 
--- MS SQL Server / MySQL
+-- MySQL
 DELETE FROM DR1
 USING DuplicateRecords DR1
 JOIN DuplicateRecords DR2 ON
@@ -16,3 +16,11 @@ JOIN DuplicateRecords DR2 ON
     DR1.Value3 = DR2.Value3 AND
     DR1.RecordID > DR2.RecordID;
 
+-- MS SQL Server
+DELETE DR1
+FROM DuplicateRecords DR1
+JOIN DuplicateRecords DR2 
+    ON DR1.Value1 = DR2.Value1 
+    AND DR1.Value2 = DR2.Value2 
+    AND DR1.Value3 = DR2.Value3 
+    AND DR1.RecordID &gt; DR2.RecordID;
