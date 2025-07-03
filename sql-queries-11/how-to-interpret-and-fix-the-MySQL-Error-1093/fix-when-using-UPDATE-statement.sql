632b1b689b367a7785b283a13315027cfc3da5fa
dvohra09 <dvohra09@yahoo.com>
2025-07-03 09:19:51 -0700

    Update fix-when-using-UPDATE-statement.sql

--- sql-queries-11/how-to-interpret-and-fix-the-MySQL-Error-1093/fix-when-using-UPDATE-statement.sql
+++ sql-queries-11/how-to-interpret-and-fix-the-MySQL-Error-1093/fix-when-using-UPDATE-statement.sql
@@ -1,37 +1,34 @@
 -- This statement generates the Error 1093
-UPDATE Program 
-SET end_date = (SELECT start_date FROM Program);
+UPDATE Program t 
+SET t.end_date = (SELECT start_date FROM Program WHERE id=t.id);
 
 -- This statement fixes the Error 1093 in the preceding example using a derived table
-UPDATE Program
-SET end_date = (
-  SELECT subquery_program.start_date
-  FROM (
-    SELECT start_date
-    FROM Program 
-    WHERE description ='Major in Operating Systems'
+UPDATE Program t 
+SET t.end_date = (
+  SELECT subquery_program.start_date FROM (
+    SELECT id, start_date FROM Program WHERE id=t.id
   ) AS subquery_program
 );
 
--- This statement generates the Error 1093
-UPDATE Program
-SET name = (SELECT description FROM Program);
-
--- This set of statements fixes the Error 1093 in the preceding example using a temporary table
+-- This set of statements fixes the Error 1093 in the same example using a temporary table
 -- Create a temporary table 
-CREATE TEMPORARY TABLE IF NOT EXISTS temp_table AS
-SELECT id, description
+CREATE TEMPORARY TABLE 
+IF NOT EXISTS temp_table 
+AS SELECT id, start_date 
 FROM Program;
 
 -- Update the main table using the temporary table
-UPDATE Program t
-JOIN temp_table temp ON t.id = temp.id
-SET t.name = temp.description;
+UPDATE Program t 
+JOIN temp_table temp 
+ON t.id = temp.id 
+SET t.end_date = temp.start_date;
 
 -- Drop the temporary table
-DROP TEMPORARY TABLE IF EXISTS temp_table;
+DROP TEMPORARY TABLE 
+IF EXISTS temp_table;
 
 -- This statement fixes the Error 1093 using a self-join
-UPDATE Program p1
-JOIN Program p2 ON p1.id = p2.id
-SET p1.name = p2.description;
+UPDATE Program p1 
+JOIN Program p2 
+ON p1.id = p2.id 
+SET p1.end_date = p2.start_date;
