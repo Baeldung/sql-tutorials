73f8c2123624f5cd5c4787e5b5b2844f70080bab
dvohra09 <dvohra09@yahoo.com>
2025-07-03 09:23:11 -0700

    Update fix-when-using-DELETE-statement.sql

--- sql-queries-11/how-to-interpret-and-fix-the-MySQL-Error-1093/fix-when-using-DELETE-statement.sql
+++ sql-queries-11/how-to-interpret-and-fix-the-MySQL-Error-1093/fix-when-using-DELETE-statement.sql
@@ -1,41 +1,36 @@
 
 -- This statement generates the Error 1093
-DELETE FROM Program
-WHERE id IN 
-(SELECT id FROM Program WHERE department_id=1);
+DELETE FROM Program 
+WHERE start_date > 
+(SELECT MIN(start_date) FROM Program);
 
 -- This statement fixes the Error 1093 in the preceding example using a derived table
 DELETE FROM Program
-WHERE id IN (
-  SELECT subquery_program.id
+WHERE start_date > (
+  SELECT subquery_program.start_date
   FROM (
-    SELECT id
-    FROM Program
-    WHERE department_id=1
+    SELECT MIN(start_date) AS start_date FROM Program
   ) AS subquery_program
 );
 
 
 -- This set of statements fixes the Error 1093 in the example using a temporary table
--- Create a temporary table 
-CREATE TEMPORARY TABLE temp_ids AS
-SELECT id 
-FROM Program 
-WHERE department_id=1;
+- Create a temporary table 
+CREATE TEMPORARY TABLE temp_min_start_date AS
+SELECT MIN(start_date) AS min_date
+FROM Program;
 
 -- Delete from the main table using the temporary table
 DELETE FROM Program
-WHERE id 
-IN (SELECT id FROM temp_ids);
+WHERE start_date > 
+(SELECT min_date FROM temp_min_start_date);
 
 -- Drop the temporary table
-DROP TEMPORARY TABLE IF EXISTS temp_ids;
+DROP TEMPORARY TABLE temp_min_start_date;
 
--- This statement fixes the Error 1093 using a self-join
-DELETE t1
-FROM Program t1
-JOIN (
-    SELECT id
-    FROM Program
-    WHERE department_id=1
-) AS t2 ON t1.id = t2.id;
+-- This statement fixes the Error 1093 using a join with a derived table
+DELETE p
+FROM Program AS p
+JOIN (SELECT MIN(start_date) AS min_start_date FROM Program) 
+AS min_program_date
+ON p.start_date > min_program_date.min_start_date;
