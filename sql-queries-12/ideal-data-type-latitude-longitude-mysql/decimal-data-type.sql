781e267fc0f9d46749138a77b8923e6771f28bd3
Deepak Vohra <dvohra10@yahoo.com>
2025-07-16 12:30:55 -0400

    Update decimal-data-type.sql

--- sql-queries-12/ideal-data-type-latitude-longitude-mysql/decimal-data-type.sql
+++ sql-queries-12/ideal-data-type-latitude-longitude-mysql/decimal-data-type.sql
@@ -1,12 +1,12 @@
 -- Create example table
-CREATE TABLE coordinates (
+CREATE TABLE Coordinates (
     id INT AUTO_INCREMENT PRIMARY KEY,
     location VARCHAR(255),
     latitude DECIMAL(10, 8) NOT NULL,
     longitude DECIMAL(11, 8) NOT NULL
 );
 -- Add example data
-INSERT INTO coordinates (location, latitude, longitude)
+INSERT INTO Coordinates (location, latitude, longitude)
 VALUES ('Eiffel Tower', 48.8584, 2.2945);
 -- Query example data
-SELECT * FROM coordinates;
+SELECT * FROM Coordinates;
