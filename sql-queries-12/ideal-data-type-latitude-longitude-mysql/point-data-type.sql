54dca331312d15feccca48e4e9888d6fb14c84dd
dvohra09 <dvohra09@yahoo.com>
2025-07-16 12:26:36 -0400

    Update point-data-type.sql

--- sql-queries-12/ideal-data-type-latitude-longitude-mysql/point-data-type.sql
+++ sql-queries-12/ideal-data-type-latitude-longitude-mysql/point-data-type.sql
@@ -1,16 +1,20 @@
 -- Create example table
-CREATE TABLE locations (
+CREATE TABLE Locations (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
     coordinates POINT NOT NULL SRID 4326,
     SPATIAL INDEX(coordinates)
 );
 -- Add example data
-INSERT INTO locations (name, coordinates)
+INSERT INTO Locations (name, coordinates)
 VALUES ('Eiffel Tower', ST_PointFromText('POINT(2.2945 48.8584)', 4326));
+INSERT INTO Locations (name, coordinates)
+VALUES ('Arc de Triomphe', ST_PointFromText('POINT(2.2950 48.8738)', 4326));
 -- Query example data
-SELECT name, 
-ST_Distance_Sphere(coordinates, 
-    ST_PointFromText('POINT(49.2827 -123.1207)', 4326)) 
-    AS distance_meters
-FROM locations;
+SELECT
+    ST_Distance_Sphere(t1.coordinates, t2.coordinates) AS Distance_in_Meters 
+FROM
+    Locations AS t1,
+    Locations AS t2 
+WHERE
+    t1.name = 'Eiffel Tower' AND t2.name = 'Arc de Triomphe';
